7cfac655e8141995225236c91cec6a89f05ef242
tnwjd4623 <lsj9556@gmail.com>
2023-08-21 14:15:47 +0900

    Loginuser Annotation 적용

--- src/main/java/com/example/springbootAWS/web/IndexController.java
+++ src/main/java/com/example/springbootAWS/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.springbootAWS.web;
 
+import com.example.springbootAWS.config.auth.LoginUser;
 import com.example.springbootAWS.config.auth.dto.SessionUser;
 import com.example.springbootAWS.service.posts.PostsService;
 import com.example.springbootAWS.web.dto.PostsResponseDto;
@@ -24,10 +25,8 @@ public class IndexController {
     private final HttpSession httpSession;
     
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
